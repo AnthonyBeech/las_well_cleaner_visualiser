a79e955ef4621b3ad06c2f718482be485d82bdfe
AnthonyBeech <abeech123@virginmedia.com>
2023-09-17 09:26:47 +0100

    modified:   test.py

--- test.py
+++ test.py
@@ -3,108 +3,88 @@ import numpy as np
 import matplotlib.pyplot as plt
 plt.rcParams.update({'font.size': 5})
 
-
-def v_from_dt(dt_log, z_log):
-    """
+class wellCleaner:
+    """Cleans wells
     """
     
-    N = len(dt_log)
-    v = [0] * N
+    # main args
+    DT_UNIT = 1000000  # us
+    Z_NM = "DEPT"
+    DT_NM = "DT"
+    RES_NM = "RESD"
     
-    for i in range(N):
-        v[i] = z_log[i] / dt_log[i] * 1000000
+    # ricker args
+    L_RICK = 0.256 # ms, half length
+    F_RICK = 50 #12 # Hz
+    DT_RICK = 0.001
+    
+    
+    def __init__(self, las_file):
+        self.las = lasio.read(FLOC)
+        print(self.las.curves)
+        self.df = self.las.df().reset_index()
+        
+        
+    def clean(self):
+        self.df["V"] = self.__v_from_dt(self.df[self.Z_NM], self.df[self.DT_NM])
+        self.df["AI"] = self.__ai_from_v_rho(self.df["V"], self.df[self.RES_NM])
+        self.df["RICK_QC"] = self.__rick_qc(self.df[self.Z_NM])
+        self.df["SYNT"] = self.__make_synt(self.df["AI"])
         
     
-    return v
-
-
-def ai_from_v_rho(v, rho):
-    """
-    """
-    
-    N = len(v)
-    ai = [0]*N
-    
-    for i in range(N):
-        ai[i] = v[i] * rho[i]
+    def print_logs(self):
+        z = self.df["DEPT"]
+        df = self.df.drop(columns="DEPT")
+        N = len(df.columns)
         
-    return v # ai
-
-
-def make_synt(ai):
-    """
-    """
-    reflectivity = np.diff(ai)
-    reflectivity = np.insert(reflectivity, 0, 0, axis=0)
-    
-    print(len(reflectivity))
-    synthetic_seismogram = np.convolve(reflectivity, make_ricker(), mode='same')
-    print(len(synthetic_seismogram))
-    
-    old_indices = np.linspace(0, len(reflectivity)-1, num=len(reflectivity))
-    new_indices = np.linspace(0, len(reflectivity)-1, num=len(synthetic_seismogram))
-
-    resampled_signal = np.interp(old_indices, new_indices, synthetic_seismogram)
-    
-    return resampled_signal
-
-
-def rick_qc(z):
-    """
-    """
-    L = len(z)
-    rick = make_ricker()
-    old_indices = np.linspace(0, len(rick)-1, num=len(rick))
-    new_indices = np.linspace(0, len(rick)-1, num=L)
-
-    resampled_signal = np.interp(new_indices, old_indices, rick)
-    
-    return resampled_signal
-
-
-def make_ricker():
-    """
-    """
-    L = 0.256 # ms, half length
-    pi = np.pi
-    f = 50 #12 # Hz
-    dt = 0.001
-    
-    t = np.arange(-L / 2, (L-dt) / 2, dt)
-    print(t)
-    y = (1.0 - 2.0*(np.pi**2)*(f**2)*(t**2)) * np.exp(-(np.pi**2)*(f**2)*(t**2))
-    
-    return y
-
-
-def print_logs(df):
-    """
-    """
-    z = df["DEPT"]
-    df = df.drop(columns="DEPT")
-    N = len(df.columns)
-    
-    fig, ax = plt.subplots(1, N)
-    for i, _log in enumerate(df):
-        ax[i].plot(df[_log], z)
-        ax[i].set_title(_log)
+        fig, ax = plt.subplots(1, N)
+        for i, _log in enumerate(df):
+            ax[i].plot(df[_log], z)
+            ax[i].set_title(_log)
         
-    plt.show()
+        plt.tight_layout()
+        plt.show()
 
-    return 0
+
+    def __v_from_dt(self, dt, z):
+        return np.array(z) / np.array(dt) * self.DT_UNIT
+
+    def __ai_from_v_rho(self, v, rho):
+        return np.array(v) * np.array(rho)
+
+    def __make_synt(self, ai):
+        ref = np.diff(ai)
+        ref = np.insert(ref, 0, 0, axis=0)
+
+        synt = np.convolve(ref, self.__make_ricker(), mode='same')
+        
+        return self.__resamp(synt, ref)
+    
+    def __make_ricker(self):    
+        first = -1 * self.L_RICK / 2
+        last = (self.L_RICK - self.DT_RICK) / 2
+        
+        t = np.arange(first, last, self.DT_RICK)
+        
+        # formula for ricker wavelet
+        wav = ((1.0 - 2.0*(np.pi**2)*(self.F_RICK**2)*(t**2)) 
+               * np.exp(-(np.pi**2)*(self.F_RICK**2))*(t**2))
+        
+        return wav
+
+    def __rick_qc(self, z):
+        rick = self.__make_ricker()
+        return self.__resamp(rick, z)
+    
+    def __resamp(s, s_target):
+        old_i = np.linspace(0, len(s_target)-1, num=len(s))
+        new_i = np.linspace(0, len(s_target)-1, num=len(s_target))
+
+        return np.interp(new_i, old_i, s)
 
 
 FLOC = "test_well.las"
-las = lasio.read(FLOC)
-print(las.curves)
 
-make_ricker()
-
-df = las.df().reset_index()
-
-df["V"] = v_from_dt(df["DEPT"], df["DT"])
-df["AI"] = ai_from_v_rho(df["V"], df["RESD"])
-df["RICK_QC"] = rick_qc(df["DEPT"])
-df["SYNT"] = make_synt(df["AI"])
-
-print_logs(df)
+wells = wellCleaner(FLOC)
+wells.clean()
+wells.print_logs()
